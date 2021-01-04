f08f238e55a2dad3c88b95e0215d436ab1b359b1
JuanEscurra <archys.wow@gmail.com>
2021-01-04 00:14:05 -0500

    article-preview 1.0

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -3,15 +3,17 @@ let abierto = false;
 btnShare.addEventListener('click', () => {
 
     const boxShare = document.getElementById('boxShare');
+    const iconPath = document.getElementById('iconShare');
     
     if(abierto === false) {
         boxShare.style.display = "block";
         btnShare.style.backgroundColor = "var(--DesaturatedDarkBlue)";
-        btnShare.style.color = "black";
+        iconPath.style.fill = "var(--LightGrayishBlue)";
         abierto = true;
     } else {
         boxShare.style.display = "none";
         btnShare.style.backgroundColor = "var(--LightGrayishBlue)";
+        iconPath.style.fill = "var(--DesaturatedDarkBlue)";
         abierto = false;
     }
 });
